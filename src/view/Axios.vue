7145b34523516d93bbd08592e276e4a877676887
Chia <lovemaple0913@gmail.com>
2021-10-21 15:44:02 +0800

    Init

---
+++ src/view/Axios.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// defineProps<{ msg: string }>()
+
+const testMsg = ref("Axios View");
+</script>
+
+<template>
+  <div>{{ testMsg }}</div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src") // 設置 '@' 指向 'src' 目錄
+    }
+  },
+  base: "./", // 設置打包路徑
+  server: {
+    port: 8010,
+    open: false,
+    cors: true
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
+  }
+});
